06cfc2c988726f8088a567e2f00fa602bfadc4ce
Andrew Davis <atd285@gmail.com>
2020-12-09 17:57:43 -0500

    solution for part one of day 2

--- 2020/02/code_2.js
+++ 2020/02/code_2.js
@@ -1,4 +1,19 @@
-const fs = require('fs');
+(async function main() {
+  const fs = await require('fs');
+  const passwords = extractData();
+  const check = passwords
+    .map((line, i) => {
+      const count = line.sequence[line.letter];
+
+      if (count >= line.min && count <= line.max && count !== undefined) {
+        return { ...line, valid: true };
+      }
+      return { ...line, valid: false };
+    })
+    .filter((x) => x.valid === true).length;
+  console.log(check);
+})();
+
 function extractData() {
   return fs
     .readFileSync(require('path').join(__dirname, 'input.txt'))
@@ -6,10 +21,23 @@ function extractData() {
     .split('\n')
     .map((line) => {
       let [minMax, letter, sequence] = line.split(' ');
+      const minMaxFormat = minMax.split('-');
       return {
-        minMax,
-        letter,
-        sequence,
+        min: Number(minMaxFormat[0]),
+        max: Number(minMaxFormat[1]),
+        letter: letter.replace(':', ''),
+        sequence: checkForValid(sequence),
       };
     });
+}
+
+function checkForValid(inputArray) {
+  return [...inputArray].reduce((acc, curr) => {
+    if (acc[curr] === undefined) {
+      acc[curr] = 1;
+    } else {
+      acc[curr] += 1;
+    }
+    return acc;
+  }, {});
 }
