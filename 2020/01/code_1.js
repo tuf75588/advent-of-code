6be6df42d728beb7d225e8d97feeddc8c7bc6543
Andrew Davis <atd285@gmail.com>
2020-12-08 21:24:59 -0500

    finish solution for part 2 of day one

--- 2020/01/code_1.js
+++ 2020/01/code_1.js
@@ -1,7 +1,11 @@
+const fs = require('fs');
+
 (function main() {
   try {
     const numbersInput = extractTextInput();
     const map_two = sequenceTwoForAddition(numbersInput);
+    let a = sequenceForThree(numbersInput).reduce((a, b) => a * b);
+    console.log(a);
   } catch (error) {
     console.error(error);
   }
@@ -22,4 +26,22 @@ function sequenceTwoForAddition(nums) {
       return nums[i] * difference;
     }
   }
+}
+
+// nested for loops for answer requiring three numbers
+
+// this will need refactoring as I wager this computation is needlessly expensive
+function sequenceForThree(array) {
+  let diffs = [];
+  let output = [];
+  for (let i = 0; i < array.length - 2; i++) {
+    for (let j = i + 1; j < array.length - 1; j++) {
+      for (let k = j + 1; k < array.length; k++) {
+        if (array[i] + array[j] + array[k] === 2020) {
+          output.push(array[i], array[j], array[k]);
+        }
+      }
+    }
+  }
+  return output;
 }
