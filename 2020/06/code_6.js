c0b53cf235d687d68e6ceecdc974a20cc70777ec
Andrew Davis <atd285@gmail.com>
2020-12-15 15:59:03 -0500

    part 1 solution day 6

--- 2020/06/code_6.js
+++ 2020/06/code_6.js
@@ -2,32 +2,45 @@ const { readFile } = require('fs/promises');
 const path = require('path').join(__dirname, 'input.txt');
 (async function init() {
   const input = await readFile(path, { encoding: 'utf-8' });
-  const ws = /\s/g;
   const sampleField = `
   abc
 
   a
   b
   c
-  
+
   ab
   ac
-  
-  a
-  a
-  a
-  a
-  
-  b`;
 
+  a
+  a
+  a
+  a
+
+  b
+  `;
   //array?
-  const sample = sampleField
+  const groups = input
     .trim()
     .split('\n\n')
-    .map((group) => group.split('\n').map((inner) => inner.trim()));
-  const innerArray = sample[1].map((x) =>
-    x.trim().replace(/./, (char) => {
-      console.log(char);
-    })
-  );
+    .map((group) => group.split('\n'));
+  const letterCount = groups.map(countLetters).reduce((a, b) => a + b, 0);
+  // answer is 6578
 })();
+
+/** @param string[] group - individual array element full of strings */
+
+function countLetters(group) {
+  const letterSet = new Set();
+  for (const char of group) {
+    for (const row of char) {
+      letterSet.add(row);
+    }
+  }
+  letterSet.forEach((set) => {
+    if (set === ' ') {
+      letterSet.delete(' ');
+    }
+  });
+  return letterSet.size;
+}
