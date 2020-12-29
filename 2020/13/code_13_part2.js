12e7e21decf434b588fa748199f590b665324aa5
Andrew Davis <atd285@gmail.com>
2020-12-28 19:47:15 -0500

    day 13 part 2 solution

---
+++ 2020/13/code_13_part2.js
@@ -0,0 +1,24 @@
+const { EALREADY } = require('constants');
+const fs = require('fs');
+const { parse } = require('path');
+const path = require('path').join(__dirname, 'input.txt');
+
+const sequence = fs.readFileSync(path).toString().split('\n');
+
+let buses = sequence[1]
+  .split(',')
+  .map((num, idx) => ({ num, idx }))
+  .filter(({ num }) => num !== 'x')
+  .map(({ num, idx }) => ({ num: parseInt(num), idx }));
+
+let earliestTime = 0;
+let prod = 1;
+
+for (let i = 0; i < buses.length; i++) {
+  while ((earliestTime + buses[i].idx) % buses[i].num !== 0) {
+    earliestTime += prod;
+  }
+  prod *= buses[i].num;
+}
+
+console.log(earliestTime);
